b5dc9be7468868525da4fb9aefe5d584e5b86f50
Praveen Dias <praveendias1180@gmail.com>
2022-01-22 09:50:13 +0530

    The flud grid :+1:

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 window.onload = () => {
     const grid = document.querySelector('.grid');
-    const masonry = new Masonry(grid);
+    const masonry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        gutter: 10,
+    });
 }
